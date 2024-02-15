9d5dd6142207b244d66ca5b80e58d4534fd62665
danielcristho <pepuhodaniel93.learn@gmail.com>
2024-02-15 20:34:31 +0700

    feat: initilize flask project

--- virtualbox/ubuntu/lamp/script.sh
+++ virtualbox/ubuntu/lamp/script.sh
@@ -33,7 +33,7 @@ sudo service mysql restart
 
 # Install PHP
 sudo add-apt-repository -y ppa:ondrej/php
-sudo apt-get install libapache2-mod-php php8.2 php8.2-common php8.2-xml php8.2-mysql php8.2-gd php8.2-mbstring php8.2-tokenizer php8.2-bcmath php8.2-curl php8.2-zip unzip -y
+sudo apt-get install libapache2-mod-php php8.2 php8.2-fpm php8.2-common php8.2-xml php8.2-mysql php8.2-gd php8.2-mbstring php8.2-tokenizer php8.2-bcmath php8.2-curl php8.2-zip unzip -y
 
 # Install Composer
 curl -sS https://getcomposer.org/installer | php
